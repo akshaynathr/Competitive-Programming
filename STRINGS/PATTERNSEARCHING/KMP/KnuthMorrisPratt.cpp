2731993c1c04836fdcf31d55beef0bd5b9a522e4
Akshaynath R <akshaynathr@gmail.com>
2019-01-13 17:28:43 +0530

    Added preprocess function to compute LPS array

--- STRINGS/PATTERNSEARCHING/KMP/KnuthMorrisPratt.cpp
+++ STRINGS/PATTERNSEARCHING/KMP/KnuthMorrisPratt.cpp
@@ -2,33 +2,35 @@
 using namespace std;
 
 
-int *prepocess(int * pat,int M,int len)
+void preprocess(char * pat,int M,int *lps)
 {
-    int lps[10]; 
     int i=1; //starts from 2nd char in pattern
 
     int j=0;
     int lpscount=0;
+    lps[0]=0;
 
     while(i<M)
     {
         if(pat[i]==pat[j])
         {
             lpscount=lpscount+1;
+            lps[i]=lpscount;
             i++;
             j++;
-            lps[i]=lpscount;
+            
         }
         else
         {
             if(j!=0)
             {
-                j=j-1;
+                j=lps[j-1];
             }
 
             else
             {
                 lpscount=0;
+                lps[i]=0;
                 j=i;
                 i++;
             }
@@ -37,13 +39,19 @@ int *prepocess(int * pat,int M,int len)
 
     }
 
-    int *p = lps;
 
-    return p;
      
 }
 int main()
 {
+    char pat[] ="AAACAAAAAC";
+    int  lps [6]={0};
+    int len= sizeof(pat)/sizeof(*pat);
+    preprocess(pat,len,lps);
+    for(int i=0;i<len;i++)
+    {
+        cout<<lps[i]<<endl;
+    }
 
     return 0;
     
